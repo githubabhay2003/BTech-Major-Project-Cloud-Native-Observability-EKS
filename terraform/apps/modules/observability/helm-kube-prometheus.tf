711be81ebee933b1ae60a57fff6821da8e8149b2
Abhay Kumar Saini <abhaykumarsaini9982@gmail.com>
2026-04-11 16:56:58 +0530

    Add website ingress

--- terraform/apps/modules/observability/helm-kube-prometheus.tf
+++ terraform/apps/modules/observability/helm-kube-prometheus.tf
@@ -4,75 +4,96 @@ resource "helm_release" "kube_prometheus_stack" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
   version    = "81.2.2"
+
   depends_on = [
     kubernetes_namespace_v1.monitoring
   ]
 
+  values = [
+    yamlencode({
 
-  values = [yamlencode({
+      grafana = {
+        adminUser     = "admin"
+        adminPassword = "prom-operator"
 
-    grafana = {
-      adminUser     = "admin"
-      adminPassword = "prom-operator"
+        "grafana.ini" = {
+          server = {
+            root_url            = "http://a6b30234174294ddc819d213eac8e371-1913973493.us-east-1.elb.amazonaws.com/grafana"
+            serve_from_sub_path = true
+          }
+        }
 
-      sidecar = {
-        dashboards = {
-          enabled           = true
-          label             = "grafana_dashboard"
-          folder            = "/var/lib/grafana/dashboards/default"
-          defaultFolderName = "default"
-          searchNamespace   = "ALL"
+        sidecar = {
+          dashboards = {
+            enabled           = true
+            label             = "grafana_dashboard"
+            folder            = "/var/lib/grafana/dashboards/default"
+            defaultFolderName = "default"
+            searchNamespace   = "ALL"
+          }
+        }
+
+        dashboardProviders = {
+          "dashboardproviders.yaml" = {
+            apiVersion = 1
+            providers = [{
+              name            = "default"
+              orgId           = 1
+              folder          = ""
+              type            = "file"
+              disableDeletion = false
+              editable        = true
+              options = {
+                path = "/var/lib/grafana/dashboards/default"
+              }
+            }]
+          }
         }
       }
 
-      dashboardProviders = {
-        "dashboardproviders.yaml" = {
-          apiVersion = 1
-          providers = [{
-            name            = "default"
-            orgId           = 1
-            folder          = ""
-            type            = "file"
-            disableDeletion = false
-            editable        = true
-            options = {
-              path = "/var/lib/grafana/dashboards/default"
-            }
-          }]
+      prometheus = {
+        prometheusSpec = {
+          externalUrl = "http://a6b30234174294ddc819d213eac8e371-1913973493.us-east-1.elb.amazonaws.com/prometheus"
+          routePrefix = "/prometheus"
         }
       }
-    }
 
-    alertmanager = {
-      enabled = true
+      alertmanager = {
+        enabled = true
 
-      config = {
-        global = {}
-
-        route = {
-          receiver        = "null-receiver"
-          group_by        = ["alertname"]
-          group_wait      = "30s"
-          group_interval  = "5m"
-          repeat_interval = "4h"
+        alertmanagerSpec = {
+          externalUrl = "http://a6b30234174294ddc819d213eac8e371-1913973493.us-east-1.elb.amazonaws.com/alertmanager"
+          routePrefix = "/alertmanager"
         }
 
-        receivers = [
-          {
+        config = {
+          global = {}
+
+          route = {
+            receiver        = "null-receiver"
+            group_by        = ["alertname"]
+            group_wait      = "30s"
+            group_interval  = "5m"
+            repeat_interval = "4h"
+          }
+
+          receivers = [
+            {
               name = "null"
             },
-          {
-            name = "null-receiver"
+            {
+              name = "null-receiver"
 
-            webhook_configs = [
-              {
-                url = "http://127.0.0.1:5001/"
-              }
-            ]
-          }
-        ]
+              webhook_configs = [
+                {
+                  url = "http://127.0.0.1:5001/"
+                }
+              ]
+            }
+          ]
+        }
       }
-    }
 
-  })]
+    })
+  ]
 }
