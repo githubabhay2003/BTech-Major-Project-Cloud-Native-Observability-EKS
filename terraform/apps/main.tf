e319ff20425e7bfa3552acbe33a380a213c06552
Abhay Kumar Saini <abhaykumarsaini9982@gmail.com>
2026-04-13 15:48:58 +0530

    remove fastapi terraform module (single owner fix)

--- terraform/apps/main.tf
+++ terraform/apps/main.tf
@@ -8,23 +8,7 @@ module "observability" {
   }
 }
 
-module "fastapi_app" {
-  source = "./modules/fastapi-app"
 
-  environment             = "dev"
-  monitoring_namespace    = module.observability.monitoring_namespace
-  app_namespace           = "observability"
-  prometheus_release_name = module.observability.prometheus_release_name
-  ecr_repository_url      = data.terraform_remote_state.infra.outputs.ecr_repository_url
-  providers = {
-    kubernetes = kubernetes
-    helm       = helm
-  }
-
-  depends_on = [
-    module.observability
-  ]
-}
 
 module "ingress_nginx" {
   source = "./modules/ingress-nginx"
--- terraform/apps/modules/fastapi-app/kubernetes-namespaces.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_namespace_v1" "observability" {
-  metadata {
-    name = "observability"
-    labels = {
-      environment = var.environment
-      managed_by  = "terraform"
-    }
-  }
-}
